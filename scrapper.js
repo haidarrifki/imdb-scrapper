810b2a5ea6e9e60e7213f46dc2092b6cc9ee5b5c
Haidar Rifki <3395046-haidarrifki@users.noreply.gitlab.com>
2022-11-08 14:00:40 +0700

    add flow scrapped data save to csv

--- scrapper.js
+++ scrapper.js
@@ -1,7 +1,12 @@
 const puppeteer = require('puppeteer');
 const url = process.argv[2];
+const { stringify } = require('csv-stringify');
+const { createWriteStream } = require('fs');
+
+const columns = ['review', 'sentiment'];
 
 async function scrap() {
+  console.log('>>> Executed.');
   const browser = await puppeteer.launch({
     headless: true,
     waitUntil: 'documentloaded',
@@ -16,41 +21,60 @@ async function scrap() {
     ],
   });
 
+  // csv stringifier
+  const stringifier = stringify({ header: true, columns });
+
   console.log(url);
 
   const page = await browser.newPage();
   await page.goto(url);
 
   const loadMoreSelector = '#load-more-trigger';
-  for (let i = 0; i < 2; i++) {
-    await page.waitForSelector(loadMoreSelector);
-    await page.click(loadMoreSelector);
-    await page.waitForTimeout(2000);
+  // get all data until button nya gak ada
+  for (let i = 0; i < 999; i++) {
+    try {
+      await page.waitForSelector(loadMoreSelector);
+      await page.click(loadMoreSelector);
+      await page.waitForTimeout(2000);
+    } catch (error) {
+      break;
+    }
   }
 
-  const reviewSelector = `#main > section > div.lister > div.lister-list > div`;
+  const titleSelector = `#main > section > div.subpage_title_block > div > div > h3 > a`;
+  const titleElement = await page.$(titleSelector);
+  const titleText = await (
+    await titleElement.getProperty('textContent')
+  ).jsonValue();
 
-  const reviews = await page.$$eval(reviewSelector, (list) => {
+  const reviewSelector = `#main > section > div.lister > div.lister-list > div`;
+  const reviews = await page.$$eval(reviewSelector, async (list) => {
     const results = [];
     for (let index = 0; index < list.length; index++) {
-      const usernameSelector = `#main > section > div.lister > div.lister-list > div:nth-child(${
-        index + 1
-      }) > div.review-container > div.lister-item-content > div.display-name-date > span.display-name-link > a`;
-      const headlineSelector = `#main > section > div.lister > div.lister-list > div:nth-child(${
-        index + 1
-      }) > div.review-container > div.lister-item-content > a`;
       const reviewSelector = `#main > section > div.lister > div.lister-list > div:nth-child(${
         index + 1
       }) > div.review-container > div.lister-item-content > div.content > div.text.show-more__control`;
-      const timestampSelector = `#main > section > div.lister > div.lister-list > div:nth-child(${
+      const ratingSelector = `#main > section > div.lister > div.lister-list > div:nth-child(${
         index + 1
-      }) > div.review-container > div.lister-item-content > div.display-name-date > span.review-date`;
+      }) > div.review-container > div.lister-item-content > div.ipl-ratings-bar > span > span:nth-child(2)`;
+
+      const ratingElement = document.querySelector(ratingSelector)?.textContent;
+
+      if (ratingElement === null) {
+        continue;
+      }
+
+      const rating = parseInt(ratingElement);
+      let sentiment = '';
+      if (rating > 6) {
+        sentiment = 'positive';
+      } else {
+        sentiment = 'negative';
+      }
 
       const data = {
-        username: document.querySelector(usernameSelector).textContent.trim(),
-        headline: document.querySelector(headlineSelector).textContent.trim(),
         review: document.querySelector(reviewSelector).textContent.trim(),
-        timestamp: document.querySelector(timestampSelector).textContent.trim(),
+        sentiment: sentiment,
       };
       results.push(data);
     }
@@ -58,7 +82,17 @@ async function scrap() {
     return results;
   });
 
-  console.log(reviews);
+  for (let i = 0; i < reviews.length; i++) {
+    const review = reviews[i];
+    stringifier.write(review);
+  }
+
+  const title = titleText.replaceAll(' ', '_');
+  const filename = `output/${title}.csv`;
+  const writeableStream = createWriteStream(filename);
+  stringifier.pipe(writeableStream);
+
+  console.log('>>> Completed.');
   console.log('Total Data:', reviews.length);
 
   await page.close();
